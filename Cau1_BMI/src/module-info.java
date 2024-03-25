bd0ece59bacdf82574ed9e4c0153640227cc8dc8
Võ Tấn Thành <thanh.vt.63cntt@ntu.edu.vn>
2024-03-25 19:31:39 +0700

    update

--- Cau1_BMI/src/module-info.java
+++ Cau1_BMI/src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module Cau1_BMI {
+	requires java.desktop;
 }
---
+++ Cau1_BMI/src/ntu_63131270/BMICalculator.java
@@ -0,0 +1,13 @@
+package ntu_63131270;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class BMICalculator extends JFrame implements ActionListener {
+    private JLabel weightLabel, heightLabel, ageLabel, genderLabel, resultLabel;
+    private JTextField weightField, heightField, ageField;
+    private JRadioButton maleRadio, femaleRadio;
+    private JButton calculateButton;
+
+}
